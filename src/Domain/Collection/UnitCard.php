640962a7410be071ab89cb63097b533b3f6584fc
Ignasi Luengo Rama <iluengo@ext.wanup.com>
2018-02-05 16:14:59 +0100

    added unitcard attributes

--- src/Domain/Collection/UnitCard.php
+++ src/Domain/Collection/UnitCard.php
@@ -2,12 +2,96 @@
 
 namespace App\Domain\Collection;
 
+use App\Domain\CardId;
+use App\Domain\Exception\UnitCardInvalidLivesException;
+
 /**
  * Class UnitCard
  * @package App\Domain\Collection
  */
 class UnitCard implements \JsonSerializable
 {
+    /**
+     * @var CardId
+     */
+    private $cardId;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var int
+     */
+    private $attack;
+
+    /**
+     * @var int
+     */
+    private $lives = 0;
+
+    /**
+     * UnitCard constructor.
+     * @param CardId $cardId
+     * @param string $name
+     * @param int $attack
+     * @param int $lives
+     */
+    public function __construct(CardId $cardId, $name, $attack, $lives)
+    {
+        $this->cardId = $cardId;
+        $this->name = $name;
+        $this->attack = $attack;
+        $this->setLives($lives);
+    }
+
+    private function setLives(int $lives)
+    {
+        if ($lives < 0) {
+            throw new UnitCardInvalidLivesException("Lives must be greater than zero");
+        }
+    }
+
+    /**
+     * @return CardId
+     */
+    public function getCardId(): CardId
+    {
+        return $this->cardId;
+    }
+
+    /**
+     * @param int $attack
+     */
+    public function damage(int $attack)
+    {
+        $this->setLives($this->getLives() - $attack);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAttack(): int
+    {
+        return $this->attack;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLives(): int
+    {
+        return $this->lives;
+    }
 
     /**
      * Specify data which should be serialized to JSON
@@ -18,6 +102,9 @@ class UnitCard implements \JsonSerializable
      */
     function jsonSerialize()
     {
-        // TODO: Implement jsonSerialize() method.
+        return [
+            'id' => $this->getCardId()->getId(),
+            'name' => $this->getName()
+        ];
     }
 }
---
+++ src/Domain/Exception/UnitCardInvalidLivesException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Domain\Exception;
+
+/**
+ * Class UnitCardInvalidLivesException
+ * @package App\Domain\Exception
+ */
+class UnitCardInvalidLivesException extends \Exception
+{
+
+}
